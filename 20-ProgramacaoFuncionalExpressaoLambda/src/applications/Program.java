a7631903c04519bdc295dbf726e71819d799833e
AVogelsanger <voguednj@hotmail.com>
2021-01-05 23:25:14 -0300

    aula lambda função anônima

--- 20-ProgramacaoFuncionalExpressaoLambda/src/applications/Program.java
+++ 20-ProgramacaoFuncionalExpressaoLambda/src/applications/Program.java
@@ -16,7 +16,33 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		//list.sort();
+		
+		/*1 exemplo de uma formar de fazer
+		Comparator<Product> comparator = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};*/
+		
+		/*2 Usando uma função anônima com lambda (arrow function);
+		Comparator<Product> comparator = (p1,p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		};*/
+		
+		/*3 Usando uma função de dois argumentos do tipo function
+		Comparator<Product> comparator = (p1,p2) -> p1.getName()
+													.toUpperCase()
+													.compareTo(
+															p2.getName()
+															.toUpperCase());
+		
+		list.sort(comparator);
+		*/
+		
+		// Expressão lambda corresponde a uma função anônima de primeira classe;
+		// 4 Usando uma expressão lambda (função anônima) como argumento;
+		list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for (Product product : list) {
 			System.out.println(product);
